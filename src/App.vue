007fb58952b6407052ec8f24fadcd12faa5f5c3b
mstszk <suzuki.m.htym@gmail.com>
2019-11-06 21:59:56 +0900

    add transition

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,25 @@
-<template>
-  <div id="app">
-    <router-view/>
-  </div>
+<template lang="pug">
+  #app
+    transition(mode="out-in")
+      router-view
 </template>
 
 <style src="../node_modules/reveal.js/css/reset.css"></style>fg
 <style src="./styles/base.styl" lang="stylus"></style>
+<style lang="stylus" scoped>
+.v-enter
+	transform translate(-100px, 0)
+	opacity 0
+.v-enter-to
+	opacity 1
+.v-enter-active
+	transition all 1s 0s ease
+.v-leave
+	transform translate(0, 0)
+	opacity 1
+.v-leave-to
+	transform translate(100px, 0)
+	opacity 0
+.v-leave-active
+	transition all .5s 0s ease
+</style>
