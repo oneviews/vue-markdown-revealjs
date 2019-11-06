1856e8586f92732084319441e57daa597184c74d
mstszk <suzuki.m.htym@gmail.com>
2019-11-06 21:56:44 +0900

    add home button to not-found page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,3 +1,14 @@
 <template lang="pug">
-  h1 404
+  .not-found
+    h2 404
+    router-link(to="/") Home
 </template>
+
+<style lang="stylus" scoped>
+.not-found
+  padding 2rem
+  h2
+    font-size 1.4rem
+    font-weight bold
+    margin-bottom 1rem
+</style>
